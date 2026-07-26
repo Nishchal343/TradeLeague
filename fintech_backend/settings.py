8279e1f976137aba2635a8446cbcc4798549a581
Nishchal-K <nishchalk127@gmail.com>
2026-07-26 22:02:56 +0530

    Add readme and fix cors error

--- fintech_backend/settings.py
+++ fintech_backend/settings.py
@@ -28,7 +28,8 @@ ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS = os.getenv(
     "CSRF_TRUSTED_ORIGINS",
-    "http://localhost,http://127.0.0.1"
+    "http://localhost,http://127.0.0.1",
+    "https://tradeleague-vk4b.onrender.com",
 ).split(",")
 
 # --------------------------------------------------
