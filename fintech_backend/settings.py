27c3f0e9f8a8fd9edbe23792a143dce5c2d5421e
Nishchal-K <nishchalk127@gmail.com>
2026-07-26 22:20:18 +0530

    Merge origin/mainv1

--- fintech_backend/settings.py
+++ fintech_backend/settings.py
@@ -24,12 +24,14 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "yes")
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "localhost,127.0.0.1,http://127.0.0.1,https://tradeleague-vk4b.onrender.com"
+).split(",")
 
 CSRF_TRUSTED_ORIGINS = os.getenv(
     "CSRF_TRUSTED_ORIGINS",
-    "http://localhost,http://127.0.0.1",
-    "https://tradeleague-vk4b.onrender.com",
+    "http://localhost,http://127.0.0.1,https://tradeleague-vk4b.onrender.com"
 ).split(",")
 
 # --------------------------------------------------
