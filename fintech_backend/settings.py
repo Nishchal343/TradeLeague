48e41f2ce0d0e342b80c2b508d7aa8bfe0d60546
Nishchal-K <nishchalk127@gmail.com>
2026-07-26 21:10:57 +0530

    Configure Render hosts and CSRF origins

--- fintech_backend/settings.py
+++ fintech_backend/settings.py
@@ -26,7 +26,7 @@ DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "yes")
 
 ALLOWED_HOSTS = os.getenv(
     "ALLOWED_HOSTS",
-    "localhost,127.0.0.1"
+    "localhost,127.0.0.1,tradeleague-vk4b.onrender.com,.onrender.com"
 ).split(",")
 
 CSRF_TRUSTED_ORIGINS = os.getenv(
